2c139142f7f67d65ad1c32ff226ba4113505af96
David Dumas <72494200+david-dumas@users.noreply.github.com>
2021-10-26 12:17:00 +0200

    Agenda aanpassingen (knoppen + opmaak)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,48 +1,52 @@
 <template>
     <nav>
         <v-toolbar flat app color="#018245">
-            <v-toolbar-title class="text-uppercase grey--text">
+            <v-toolbar-title class="text-uppercase white--text">
                 <span class="font-weight-light">Ontime</span>
             </v-toolbar-title>
 
             <v-spacer></v-spacer>
 
-            <v-btn
-            depressed
-            color="white"
-            >
-
-        <router-link to="/" active-class="active" tag="button" exact class="nav-btn">
-                    Log uit
-        </router-link>
-</v-btn>
         <v-btn
+            class="button"
             depressed
             color="white"
             >
+            <router-link to="/" active-class="active" tag="button" exact class="nav-btn">
+                        Log uit
+            </router-link>
+        </v-btn>
 
-      <router-link to="/help" active-class="active" tag="button" exact class="nav-btn">
-                    Help
-        </router-link>
-</v-btn>
         <v-btn
+            class="button"
             depressed
             color="white"
             >
+            <router-link to="/help" active-class="active" tag="button" exact class="nav-btn">
+                        Help
+            </router-link>
+        </v-btn>
 
-      <router-link to="/agenda" active-class="active" tag="button" exact clasass="nav-btn">
-                    Agenda
-        </router-link>
-</v-btn>
         <v-btn
+            class="button"
             depressed
             color="white"
             >
+            <router-link to="/agenda" active-class="active" tag="button" exact clasass="nav-btn">
+                        Agenda
+            </router-link>
+        </v-btn>
 
-        <router-link to="/begeleider-dashboard" active-class="active" tag="button" exact class="nav-btn">
-                    Dashboard
-        </router-link>
-</v-btn>        
+        <v-btn
+            class="button"
+            depressed
+            color="white"
+            >
+            <router-link to="/begeleider-dashboard" active-class="active" tag="button" exact class="nav-btn">
+                        Dashboard
+            </router-link>
+        </v-btn>
+
         </v-toolbar>
     </nav>
 </template>
@@ -54,5 +58,8 @@ export default {
 </script>
 
 <style>
+    .button{
+        margin-right: 5px;
+    }
 
 </style>
