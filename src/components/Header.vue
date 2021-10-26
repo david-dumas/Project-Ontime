fecdd1b478431507a3abfd399071cb8757bc26c4
David Dumas <72494200+david-dumas@users.noreply.github.com>
2021-10-26 11:57:27 +0200

    knoppen header wit + git ignore verandering

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,36 +9,47 @@
 
             <v-btn
             depressed
-            color="primary"
+            color="white"
             >
-            <router-link to="/" active-class="active" tag="button" exact class="nav-btn">
-                    Log uit
-        </router-link>
-            </v-btn>
 
         <router-link to="/" active-class="active" tag="button" exact class="nav-btn">
                     Log uit
         </router-link>
+</v-btn>
+        <v-btn
+            depressed
+            color="white"
+            >
 
       <router-link to="/help" active-class="active" tag="button" exact class="nav-btn">
                     Help
         </router-link>
+</v-btn>
+        <v-btn
+            depressed
+            color="white"
+            >
 
       <router-link to="/agenda" active-class="active" tag="button" exact clasass="nav-btn">
                     Agenda
         </router-link>
+</v-btn>
+        <v-btn
+            depressed
+            color="white"
+            >
 
         <router-link to="/begeleider-dashboard" active-class="active" tag="button" exact class="nav-btn">
                     Dashboard
         </router-link>
-        
+</v-btn>        
         </v-toolbar>
     </nav>
 </template>
 
 <script>
 export default {
-    name: 'Header2'
+    name: 'Header'
 }
 </script>
 
