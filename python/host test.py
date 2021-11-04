6f91d242f9cee2a4e56880684d914be669cb504b
ghostlykraken <70856032+ghostlykraken@users.noreply.github.com>
2021-11-04 12:12:17 +0100

    cleanup excess code

--- python/host test.py
+++ python/host test.py
@@ -1,16 +1,4 @@
-import csv
 import mysql.connector
-
-
-#De inloggegevens van de database staan in een apart document in de volgende vorm: 'user','password','host','naam van de database'
-
-with open(r"C:\Users\caspe\OneDrive\Documents\GitHub\Project-Ontime\txt\mysql.txt") as f1:
-    data=csv.reader(f1,delimiter=",")
-    for row in data:
-            user=row[0]
-            password=row[1]
-            host=row[2]
-            database=row[3]
 
 print("enter client id: " )
 x = input()
@@ -18,10 +6,10 @@ x = input()
 def get_client_detail(id):
     try:
         ontimedb = mysql.connector.connect(
-            host=host,
-            database=database,
-            user=user,
-            password=password)
+            host="145.89.87.118",
+            database="ontime",
+            user="ontime",
+            password="ontime")
 
         cursor = ontimedb.cursor()
         sql_select_query = """SELECT * FROM client WHERE id_client = %s"""
