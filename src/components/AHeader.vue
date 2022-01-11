3388b91ec28a1b6605169d78a9bce0f3f10ce347
Hanne <hanne.dedeckere@student.hu.nl>
2022-01-11 16:03:21 +0100

    Helpbutton verplaats
    Helpbutton verplaats

--- src/components/AHeader.vue
+++ src/components/AHeader.vue
@@ -1,55 +1,55 @@
 <template>
-    <nav>
-        <v-toolbar flat app color="#018245">
-            <v-toolbar-title class="text-uppercase white--text">
-                <span class="font-weight-light">Ontime</span>
-            </v-toolbar-title>
+  <nav>
+    <v-toolbar flat app color="#018245">
+      <v-toolbar-title class="text-uppercase white--text">
+        <span class="font-weight-light">Ontime</span>
+      </v-toolbar-title>
 
-            <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-            <div class="help">
-
-            <Helppopup />
-
-            </div>
-
-        <v-btn
-            class="button"
-            depressed
-            color="white"
-            >
-            <router-link to="/admin-dashboard" active-class="active" tag="button" exact class="nav-btn">
-                        Dashboard
-            </router-link>
-        </v-btn>
-
-        <v-btn
-            class="button"
-            depressed
-            color="white"
-            >
-            <router-link to="/" active-class="active" tag="button" exact class="nav-btn" @click="logout">
-                        Log uit
-            </router-link>
-        </v-btn>
-
-        </v-toolbar>
-    </nav>
+      <v-btn class="button" depressed color="white">
+        <router-link
+          to="/admin-dashboard"
+          active-class="active"
+          tag="button"
+          exact
+          class="nav-btn"
+        >
+          Dashboard
+        </router-link>
+      </v-btn>
+      <div class="help">
+        <Helppopup />
+      </div>
+      <v-btn class="button" depressed color="white">
+        <router-link
+          to="/"
+          active-class="active"
+          tag="button"
+          exact
+          class="nav-btn"
+          @click="logout"
+        >
+          Log uit
+        </router-link>
+      </v-btn>
+    </v-toolbar>
+  </nav>
 </template>
 
 <script>
-import Helppopup from '../components/Helppopup'
+import Helppopup from "../components/Helppopup";
 
 export default {
-    name: 'AHeader',
-    components: {
-        Helppopup
-    }
-}
+  name: "AHeader",
+  components: {
+    Helppopup,
+  },
+};
 </script>
 
 <style>
-    .button{
-        margin-right: 5px;
-    }
+.button {
+  margin-right: 5px;
+}
 </style>
